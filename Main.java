884d0dccb8bb7b90c2262fc7671df52dd34eb6f6
Илья <ivkar2022@yandex.ru>
2022-02-20 19:04:54 +0300

    empty

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int classes = 8;
+        int tasks = 6;
+        int coffeeCups = 3;
+        int variables = coffeeCups - classes * tasks + tasks / coffeeCups + (classes + coffeeCups) * tasks;
+        System.out.println(variables);
+        Float.max()
+    }
+}
